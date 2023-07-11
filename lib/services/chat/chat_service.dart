3ed5e43e07b8c6dbfcef42d1c49f7c391c584b57
Sommos <Sommosgaming@gmail.com>
2023-07-11 05:58:48 +0100

    added sendMessage nad getMessage functions to then display to user

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,17 +1,47 @@
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/material.dart";
 
+import "../../model/message.dart";
+
 class ChatService extends ChangeNotifier {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
 
   Future<void> sendMessage(String recipientUserID, String message) async {
-    try {
-      _firebaseFirestore.collection("users").doc(recipientUserID).collection("messages").add({
-        "message": message,
-        "sender": FirebaseAuth.instance.currentUser!.uid,
-        "timestamp": DateTime.now(),
-      });
-    } on FirebaseException catch (e) {
-      throw Exception(e.code);
-    }
+    final String currentUserID = FirebaseAuth.instance.currentUser!.uid;
+    final String currentUserEmail = FirebaseAuth.instance.currentUser!.email.toString();
+    final Timestamp timestamp = Timestamp.now();
+
+    Message newMessage = Message(
+      senderID: currentUserID,
+      senderEmail: currentUserEmail,
+      recipientID: recipientUserID,
+      message: message,
+      timestamp: timestamp,
+    );
+
+    List<String> ids = [currentUserID, recipientUserID];
+    ids.sort();
+    String chatRoomID = ids.join("_");
+
+    await _firebaseFirestore
+      .collection("chat_rooms")
+      .doc(chatRoomID)
+      .collection("messages")
+      .add(newMessage.toMap());
+  }
+
+  Stream<QuerySnapshot> getMessage(String userID, String recipientID) {
+    List<String> ids = [userID, recipientID];
+    ids.sort();
+    String chatRoomID = ids.join("_");
+
+    return _firebaseFirestore
+      .collection("chat_rooms")
+      .doc(chatRoomID)
+      .collection("messages")
+      .orderBy("timestamp", descending: false)
+      .snapshots();
   }
 }
